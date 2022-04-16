d3af3bed4b86e65a58d8052af3a40f2720c0ae84
Trung Nguyen <trung1704ptit@gmail.com>
2022-04-16 15:28:00 +0700

    lesson 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import './App.css';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Oscar } from './components/Oscar';
-import { Person } from './components/Person';
-import { PersonList } from './components/PersonList';
-import { Status } from './components/Status';
+import "./App.css";
+import { Button } from "./components/Button";
+import { Container } from "./components/Container";
+import { Greet } from "./components/Greet";
+import { Heading } from "./components/Heading";
+import { Input } from "./components/Input";
+import { Oscar } from "./components/Oscar";
+import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
+import { Status } from "./components/Status";
 
 function App() {
   const nameList = [
@@ -30,12 +33,23 @@ function App() {
     <div className="App">
       {/* <Greet name="Trung" messageCount={20} isLoggedIn={true} />
       <PersonList names={nameList} /> */}
-      <Status status="error" />
+      {/* <Status status="error" />
       <Heading>Place holder</Heading>
       <Oscar>
         <Heading>heading component</Heading>
       </Oscar>
-      <Greet name='Trung' isLoggedIn={false} />
+      <Greet name='Trung' isLoggedIn={false} /> */}
+      <Button
+        handleClick={(event, id) => {
+          console.log("button click", event, id);
+        }}
+      ></Button>
+
+      <Input value="" handleChange={(event) => console.log(event)} />
+
+      <Container
+        styles={{ border: "1px solid #f1f1f1", padding: 20, display: "block" }}
+      ></Container>
     </div>
   );
 }
