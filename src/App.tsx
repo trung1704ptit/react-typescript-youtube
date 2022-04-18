edc54f24a3a200936aa7041c468079ee03b4e6dc
Trung Nguyen <trung1704ptit@gmail.com>
2022-04-18 09:19:28 +0700

    add context

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ import { Oscar } from "./components/Oscar";
 import { Person } from "./components/Person";
 import { PersonList } from "./components/PersonList";
 import { Status } from "./components/Status";
+import { Box } from './components/context/Box'
+import { ThemeContextProvider } from './components/context/ThemeContext';
 
 function App() {
   const nameList = [
@@ -32,24 +34,28 @@ function App() {
   return (
     <div className="App">
       {/* <Greet name="Trung" messageCount={20} isLoggedIn={true} />*/}
-      <PersonList names={nameList} />
+      {/* <PersonList names={nameList} /> */}
       {/* <Status status="error" />
       <Heading>Place holder</Heading>
       <Oscar>
         <Heading>heading component</Heading>
       </Oscar>
       <Greet name='Trung' isLoggedIn={false} /> */}
-      <Button
+      {/* <Button
         handleClick={(event, id) => {
           console.log("button click", event, id);
         }}
-      ></Button>
+      ></Button> */}
 
-      <Input value="" handleChange={(event) => console.log(event)} />
-
+      {/* <Input value="" handleChange={(event) => console.log(event)} /> */}
+      {/* 
       <Container
         styles={{ border: "1px solid #f1f1f1", padding: 20, display: "block" }}
-      ></Container>
+      ></Container> */}
+
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
